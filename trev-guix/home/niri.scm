309471169c05acd204e59bec9420d024ff267124
Trevor Arjeski <tmarjeski@gmail.com>
2026-05-22 22:31:00 +0300

    feat: create trevarj guix channel

---
+++ trev-guix/home/niri.scm
@@ -0,0 +1,26 @@
+(define-module (trev-guix home niri)
+  #:use-module (gnu)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services dotfiles)
+  #:use-module (gnu packages)
+  #:use-module (gnu services)
+  #:use-module (guix gexp)
+  #:use-module (trev-guix home base)
+  #:use-module (trev-guix services udiskie))
+
+(define-public %home-niri-environment
+  (home-environment
+   (packages %home-base-packages)
+   (services
+    (cons*
+     (service home-dotfiles-service-type
+              (home-dotfiles-configuration
+               (directories (list %dotfiles-directory))
+               (layout 'stow)
+               (packages
+                '("zsh" "guix" "guixboy" "niri" "gnome-topbar"
+                  "icons" "fuzzel" "hypr" "X"))
+               (excluded '("\\.zshenv" "\\.zshrc" "\\.zprofile"))))
+     (service udiskie-service-type)
+     %home-base-services))))
---
+++ trev-guix/packages/fonts.scm
@@ -0,0 +1,42 @@
+(define-module (trev-guix packages fonts)
+  #:use-module (gnu)
+  #:use-module (gnu packages fonts)
+  #:use-module (guix)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system font)
+  #:use-module (guix build-system trivial)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public font-iosevka-jbm
+  (package
+    (name "font-iosevka-jbm")
+    (version "1.0.0")
+    (source (local-file "../files/fonts-IosevkaJbm" #:recursive? #t))
+    (build-system font-build-system)
+    (description "My custom Iosevka font inspired by JetBrains Mono.")
+    (home-page "https://www.nerdfonts.com/")
+    (synopsis "Jetbrains Mono Iosevka variant")
+    (license license:expat)))
+
+(define-public font-iotrevka
+  (package
+    (name "font-iotrevka")
+    (version "1.0.0")
+    (source (local-file "../files/fonts-Iotrevka" #:recursive? #t))
+    (build-system font-build-system)
+    (description "My custom Iosevka font where I pick what I like.")
+    (home-page "https://www.nerdfonts.com/")
+    (synopsis "Trev's Iosevka variant")
+    (license license:expat)))
+
+(define-public font-cryptofont
+  (package
+    (name "font-cryptofonts")
+    (version "1.0.0")
+    (source (local-file "../files/cryptofont.ttf"))
+    (build-system font-build-system)
+    (description "A font for cryptocurrency symbols")
+    (home-page "N/A")
+    (synopsis "A font for cryptocurrency symbols")
+    (license license:expat)))
